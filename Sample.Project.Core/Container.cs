07b1fa995241119d3d47bbb384bb1c628e0170b9
TontonTij <TontonTij@users.noreply.github.com>
2017-05-12 09:21:11 +0200

    Initial Version

---
+++ Sample.Project.Core/Container.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Composition.Hosting;
+using System.Runtime.Loader;
+
+namespace Sample.Project.Core
+{
+    public static class Container
+    {
+        public static void MefInitialization()
+        {
+            var assemblies = Directory
+                         .GetFiles(".", "*.dll", SearchOption.AllDirectories)
+                         .Select(AssemblyLoadContext.GetAssemblyName)
+                         .Select(Assembly.Load)
+                         .ToList();
+
+            var containerConfig = new ContainerConfiguration().WithAssemblies(assemblies);
+
+        }
+    }
+}
